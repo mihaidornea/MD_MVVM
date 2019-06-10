39e5683239b4985210ee925d68e99eef42d996df
Mihai <mdornea97@gmail.com>
2019-06-10 11:03:00 +0300

    Add SerializedName to fields in NewsApiResponse Remove unnecessary return from Repository::getNews()

--- app/src/main/java/com/example/wd_mvvm/repository/Repository.kt
+++ app/src/main/java/com/example/wd_mvvm/repository/Repository.kt
@@ -1,11 +1,9 @@
 package com.example.wd_mvvm.repository
 
-import androidx.lifecycle.MutableLiveData
 import com.example.wd_mvvm.models.NewsApiResponse
 import com.example.wd_mvvm.repository.remote.RemoteDataSourceImpl
 import org.koin.core.KoinComponent
 import org.koin.core.inject
-import retrofit2.Call
 import retrofit2.Callback
 
 class Repository : KoinComponent {
@@ -13,6 +11,6 @@ class Repository : KoinComponent {
     private val remoteDataSource by inject<RemoteDataSourceImpl>()
 
     fun getNews(sources: String, callback: Callback<NewsApiResponse>) {
-        return remoteDataSource.getNews(sources, callback)
+        remoteDataSource.getNews(sources, callback)
     }
 }
